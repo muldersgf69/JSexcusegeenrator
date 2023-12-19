a8c2a296f44a122160c23090f961e690c2798d54
muldersgf69 <109549032+muldersgf69@users.noreply.github.com>
2023-12-19 00:07:01 +0000

    first commit

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 window.onload = () => {
     document.querySelector('#button').addEventListener("click", () => {
         document.querySelector('#excuse').innerHTML = generateExcuse(); 
-    });
+    });console.log(generateExcuse)
 };
-let generateExcuse = () => {
-    let pronoun = ['A','The'];
-    let subject = [ 'jogger', 'raccoon','dog','driver','comedian','pincone'];
-    let action = ['took my','threw my','yelled at my', 'stole my', 'bit my'];
-    let thing = ['homework','toe','car','shoe'];
-    let where = ['on the street','in my house','in my driveway'];
+function generateExcuse() {
+    let pronoun = ['A', 'The'];
+    let subject = ['jogger', 'raccoon', 'dog', 'driver', 'comedian', 'pincone'];
+    let action = ['took my', 'threw my', 'yelled at my', 'stole my', 'bit my'];
+    let thing = ['homework', 'toe', 'car', 'shoe'];
+    let where = ['on the street', 'in my house', 'in my driveway'];
 
     let proIndx = Math.floor(Math.random() * pronoun.length);
     let subjIndx = Math.floor(Math.random() * subject.length);
     let actionIndx = Math.floor(Math.random() * action.length);
     let thingIndx = Math.floor(Math.random() * thing.length);
     let whereIndx = Math.floor(Math.random() * where.length);
-    return pronoun[proIndx]+''+subject[subjIndx]+''+action[actionIndx]+''+thing[thingIndx]+''+where[whereIndx];
+    return pronoun[proIndx] + '' + subject[subjIndx] + '' + action[actionIndx] + '' + thing[thingIndx] + '' + where[whereIndx];
 
 
 
