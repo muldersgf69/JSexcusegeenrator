f728b439d6920c037f8afefd6e5dcbfa042d632f
muldersgf69 <109549032+muldersgf69@users.noreply.github.com>
2023-12-19 01:19:41 +0000

    first commit

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 function generateExcuse() {
-    var whoList = ['The dog', 'My grandma', 'His turtle', 'My bird'];
-    var actionList = ['ate', 'peed', 'crushed', 'broke'];
-    var whatList = ['my homework', 'the keys', 'the car'];
-    var whenList = ['before the class', 'right on time', 'when I finished', 'during my lunch', 'while I was praying'];
-  
-    function getRandomElement(array) {
-      return array[Math.floor(Math.random() * array.length)];
-    }
-  
-    var excuse = getRandomElement(whoList) + ' ' + getRandomElement(actionList) + ' ' + getRandomElement(whatList) + ' ' + getRandomElement(whenList) + '.';
-  
-    return excuse;
+  var whoList = ['The dog', 'My grandma', 'His turtle', 'My bird'];
+  var actionList = ['ate', 'peed', 'crushed', 'broke'];
+  var whatList = ['my homework', 'the keys', 'the car'];
+  var whenList = ['before the class', 'right on time', 'when I finished', 'during my lunch', 'while I was praying'];
+
+  function getRandomElement(array) {
+    return array[Math.floor(Math.random() * array.length)];
   }
-  function updateExcuse() {
-    var excuseElement = document.getElementById('excuse');
-    excuseElement.textContent = generateExcuse();
-  }
-  document.getElementById('button').addEventListener('click', updateExcuse);
-  window.onload = function () {
-    updateExcuse();}
+
+  var excuse = getRandomElement(whoList) + ' ' + getRandomElement(actionList) + ' ' + getRandomElement(whatList) + ' ' + getRandomElement(whenList) + '.';
+
+  return excuse;
+}
+
+function updateExcuse() {
+  var excuseElement = document.getElementById('excuse');
+  excuseElement.textContent = generateExcuse();
+}
+
+document.getElementById('button').addEventListener('click', updateExcuse);
+
+window.onload = function () {
+  updateExcuse();
+};
